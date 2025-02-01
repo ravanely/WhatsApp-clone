99d5e6ccea9f10d733d5b848378cd826e0ab548f
Fabrice Nague <nagueravanely@gmail.com>
2025-01-31 21:30:09 -0500

    Start implement main and chat list ui

--- whatsapp-clone-ui/src/app/components/chat-list/chat-list.component.ts
+++ whatsapp-clone-ui/src/app/components/chat-list/chat-list.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, input, InputSignal} from '@angular/core';
+import {ChatResponse} from '../../services/models/chat-response';
+import {DatePipe} from '@angular/common';
+import {UserResponse} from '../../services/models/user-response';
+import {UserService} from '../../services/services/user.service';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './chat-list.component.html',
   styleUrl: './chat-list.component.scss'
 })
 export class ChatListComponent {
+  chats: InputSignal<ChatResponse[]> = input<ChatResponse[]>([])
+  searchNewContact = false
+  contacts: Array<UserResponse> = [];
 
+  constructor(private userService: UserService) {
+  }
+
+  searchContact() {
+    this.userService.getAllUsersExceptSelf().subscribe({
+      next: (users) => {
+        this.contacts = users;
+        this.searchNewContact = true;
+      }
+    })
+  }
+
+  chatClicked(chat: ChatResponse) {
+
+  }
+
+  wrapMessage(lastMessage: string | undefined) {
+    if (lastMessage && lastMessage.length <= 20) {
+      return lastMessage
+    }
+    return lastMessage?.substring(0, 17) + '...';
+  }
+
+  selectContact(contact: UserResponse) {
+
+  }
 }
--- whatsapp-clone-ui/src/app/pages/main/main.component.ts
+++ whatsapp-clone-ui/src/app/pages/main/main.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ChatListComponent} from '../../components/chat-list/chat-list.component';
+import {ChatResponse} from '../../services/models/chat-response';
+import {ChatService} from '../../services/services/chat.service';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [
+    ChatListComponent
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
+  chats: Array<ChatResponse> = [];
+
+  constructor(private chatService: ChatService) {
+  }
+
+  ngOnInit(): void {
+    this.getAllChats();
+  }
+
+  private getAllChats() {
+    this.chatService.getChatsByReceiver().subscribe({
+      next: (res) => {
+        this.chats = res
+      }
+    })
+  }
 }
